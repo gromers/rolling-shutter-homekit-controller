5c18d7ace99323fa253ee13ae0bf8fd0644470c8
gromers <geert.romers@gmail.com>
2021-04-29 03:30:17 +0200

    correct version of the relay.py in the repo

--- src/controller/relay.py
+++ src/controller/relay.py
@@ -1,16 +1,16 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 class Relay: 
     def __init__(self, pin, name):
         self.pin = pin
         self.name = name
-	    # GPIO.setmode(GPIO.BCM)
-	    # GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT)
 
     def on(self):
         print("{0} ON".format(self.name))
-	    # GPIO.output(self.pin, GPIO.HIGH)
+        GPIO.output(self.pin, GPIO.HIGH)
 
     def off(self):
         print("{0} OFF".format(self.name))
-	    # GPIO.output(self.pin, GPIO.LOW)
+        GPIO.output(self.pin, GPIO.LOW)
