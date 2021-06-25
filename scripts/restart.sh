ca569ed203f36d1edf5c6832b02119faf6b199c7
gromers <gromers>
2021-06-25 13:30:29 +0200

    added echos to see what is going on

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 # Stopping the controller and the homekit service
+echo "stopping homekit"
 sudo systemctl stop homekit
+
+echo "stopping controller"
 sudo systemctl stop controller
 
 # Give the system 15 seconds to settle
+echo "sleeping to allow for shutdown"
 sleep 15
 
 # Starting the controller
+echo "starting the controller"
 sudo systemctl start controller
 
 # Sleeping 3 seconds to give the server startup time
+echo "small delay .."
 sleep 3
 
 # Start the homekit service again
+echo "starting homekit"
 sudo systemctl start homekit
+
+echo "current state:"
+sudo systemctl status controller
+sudo systemctl status homekit
