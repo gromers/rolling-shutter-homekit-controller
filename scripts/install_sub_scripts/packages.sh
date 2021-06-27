f2a95cde86a96f322ed5f5071606b03755c9d74a
gromers <geert.romers@gmail.com>
2021-06-27 14:39:11 +0200

    skipping node

--- scripts/install_sub_scripts/packages.sh
+++ scripts/install_sub_scripts/packages.sh
@@ -1,5 +1,6 @@
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-
+# Skipping node & npm for now
+#curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 #sudo apt-get install nodejs
 #sudo apt-get install npm
+
 sudo apt-get install rpi.gpio
